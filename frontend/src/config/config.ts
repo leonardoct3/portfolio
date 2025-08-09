74562c123e7349a3de171747c9c9ede3faf74c5b
leonardoct3 <leocarvalhoteixeira@gmail.com>
2025-08-09 13:56:26 -0300

    fix: update apiBaseUrl for production environment and remove unused components export

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,3 +1,3 @@
 export const config = { 
-  apiBaseUrl: window.location.origin == 'http://localhost:5173' ? "http://localhost:5000" : "URL CRIADA NO DEPLOY"
+  apiBaseUrl: window.location.origin == 'http://localhost:5173' ? "http://localhost:5000" : "https://portfolio-production-a49a.up.railway.app"
 }
